87f07f473ba2d685085bd05163f59064d2746d76
MohsenDorado <161645984+MohsenDorado@users.noreply.github.com>
2024-04-06 22:50:05 +0330

    Right side mini profile

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
+import Feed from '@/components/Feed'
+
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+<Feed/> )
 }
---
+++ src/components/MiniProfile.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function MiniProfile() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex items-center justify-between mt-14 ml-10 ">
+      <img
+        className="rounded-full w-16 h-16 border p-[2px]"
+        src={session?.user?.image || "800px-Instagram_logo_2016.webp"}
+        alt="Userpic"
+      />
+      <div className="flex-1 pl-2">
+        <h2 className="font-bold">{session?.user?.username}</h2>
+        <h3 className="text-sm text-gray-500">Welcome to mini Insta</h3>
+      </div>
+      {session ? (
+        <button
+          className="text-red-500 text-sm font-semibold hover:brightness-150"
+          onClick={signOut}
+        >Signout</button>
+      ) : (
+        <button className="text-blue-600 text-sm font-semibold hover:brightness-150" onClick={signIn}>Signin</button>
+      )}
+    </div>
+  );
+}
